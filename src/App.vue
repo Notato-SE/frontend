816079602fbc433165bb7a4374991271ba3dff18
SeakmengChheang <c.seakmeng0603@gmail.com>
2021-01-11 13:14:19 +0700

    added: vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,32 @@
 <template>
-  <v-app>
-    <v-app-bar
-      absolute
-      color="white"
-      elevate-on-scroll
-      scroll-target="#scrolling-techniques-7"
-    >
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
-
-      <v-toolbar-title>Title</v-toolbar-title>
-
-      <v-spacer></v-spacer>
-
-      <v-btn icon>
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
-
-      <v-btn icon>
-        <v-icon>mdi-heart</v-icon>
-      </v-btn>
-
-      <v-btn icon>
-        <v-icon>mdi-dots-vertical</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <HelloWorld />
-    </v-main>
-  </v-app>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>
 </template>
 
-<script>
-import HelloWorld from "./components/HelloWorld";
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 
-export default {
-  name: "App",
+#nav {
+  padding: 30px;
 
-  components: {
-    HelloWorld,
-  },
+  a {
+    font-weight: bold;
+    color: #2c3e50;
 
-  data: () => ({
-    //
-  }),
-};
-</script>
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,101 @@
+<template>
+  <!-- <v-app-bar
+    absolute
+    color="white"
+    elevate-on-scroll
+    scroll-target="#scrolling-techniques-7"
+  >
+    <v-app-bar-nav-icon @click.prevent="drawer = !drawer"></v-app-bar-nav-icon>
+
+    <v-navigation-drawer app v-model="drawer">
+      <p>Hello</p>
+    </v-navigation-drawer>
+
+    <v-toolbar-title>Title</v-toolbar-title>
+
+    <v-spacer></v-spacer>
+
+    <v-btn icon>
+      <v-icon>mdi-magnify</v-icon>
+    </v-btn>
+
+    <v-btn icon>
+      <v-icon>mdi-heart</v-icon>
+    </v-btn>
+
+    <v-btn icon>
+      <v-icon>mdi-dots-vertical</v-icon>
+    </v-btn>
+  </v-app-bar> -->
+  <nav>
+    <v-app-bar
+      app
+      absolute
+      color="white"
+      elevate-on-scroll
+      scroll-target="#scrolling-techniques-7"
+    >
+      <v-app-bar-nav-icon
+        @click.prevent="drawer = !drawer"
+      ></v-app-bar-nav-icon>
+      <v-toolbar-title>Notato</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-navigation-drawer app v-model="drawer">
+      <v-list-item-group nav>
+        <v-list-item-group active-class="deep-purple--text text--accent-4">
+          <v-list-item v-for="item in items" :key="item.title">
+            <v-list-item-icon>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list-item-group>
+    </v-navigation-drawer>
+  </nav>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    drawer: false,
+    collapseOnScroll: false,
+    items: [
+      {
+        active: true,
+        exact: true,
+        title: "Calculator",
+        icon: "mdi-heart",
+        path: "/calculator",
+      },
+      {
+        active: true,
+        exact: true,
+        title: "Converter",
+        icon: "mdi-heart",
+        path: "/converter",
+      },
+      {
+        active: true,
+        exact: true,
+        title: "Randomizer",
+        icon: "mdi-heart",
+        path: "/randomizer",
+      },
+    ],
+  }),
+};
+</script>
+<style lang="scss">
+</style>
