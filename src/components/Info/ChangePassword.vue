4da7fd9cdd959078510671ffc3cc73db73e49628
Vichea <mr.vichea.007@gmail.com>
2021-02-28 22:37:29 +0700

    Fix Bug

--- src/components/Info/ChangePassword.vue
+++ src/components/Info/ChangePassword.vue
@@ -3,12 +3,13 @@
     <div class="d-flex justify-center">
       <v-row>
         <v-col cols="12" class="d-flex justify-center">
-          <div style="width: 50%; text-align: start" class="mb-n10">
+          <div style="width: 50%; text-align: start" class="mb-n5">
             <p>Current Password</p>
             <v-text-field
               v-model="form.old_password"
               type="password"
               outlined
+              :rules="[rules.required]"
             ></v-text-field>
             <!-- :append-icon="show.old_password ? 'mdi-eye' : 'mdi-eye-off'"
               :type="show.old_password ? 'text' : 'password'"
@@ -16,7 +17,7 @@
           </div>
         </v-col>
         <v-col cols="12" class="d-flex justify-center">
-          <div style="width: 50%; text-align: start">
+          <div style="width: 50%; text-align: start" class="mb-n5">
             <p>New Password</p>
             <v-text-field
               v-model="form.new_password"
@@ -24,6 +25,7 @@
               hint="Your password must be longer than 8 characters."
               persistent-hint
               outlined
+              :rules="[rules.required]"
             ></v-text-field>
             <!-- :append-icon="show.new_password ? 'mdi-eye' : 'mdi-eye-off'"
               :type="show.new_password ? 'text' : 'password'"
@@ -31,12 +33,13 @@
           </div>
         </v-col>
         <v-col cols="12" class="d-flex justify-center">
-          <div style="width: 50%; text-align: start" class="mb-n10">
+          <div style="width: 50%; text-align: start" class="mb-n5">
             <p>Confirm New Password</p>
             <v-text-field
               v-model="form.new_password_confirmation"
               type="password"
               outlined
+              :rules="[rules.required]"
             ></v-text-field>
             <!-- :append-icon="
                 show.new_password_confirmation ? 'mdi-eye' : 'mdi-eye-off'
@@ -48,7 +51,13 @@
           </div>
         </v-col>
         <v-col cols="12" class="d-flex justify-center">
-          <v-btn @click="save()" elevation="2" class="mt-2" color="primary">
+          <v-btn
+            @click="save()"
+            elevation="2"
+            class="mt-2"
+            :class="canSave(form) ? '' : 'primary'"
+            :disabled="canSave(form)"
+          >
             Change Password
           </v-btn>
         </v-col>
@@ -71,7 +80,21 @@ export default {
       new_password: false,
       new_password_confirmation: false,
     },
+    rules: {
+      required: (value) => !!value || "This field is required.",
+    },
   }),
+  computed: {
+    canSave() {
+      return (form) => {
+        return !(
+          form.old_password.length &&
+          form.new_password.length &&
+          form.new_password_confirmation.length
+        );
+      };
+    },
+  },
   methods: {
     async save() {
       await this.postAxios("/change-password", this.form, "put");
